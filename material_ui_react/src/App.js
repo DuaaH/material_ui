97dc21cce54d28c422fc2b3f090f66ebbca22a27
DuaaH <dodohalaiqa.1@gmail.com>
2020-05-10 17:27:25 +0300

    delete the components

--- material_ui_react/src/App.js
+++ material_ui_react/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import NavBar from '../src/components/NavBar'
-import CourseList from '../src/components/CourseList'
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <CourseList />
+      Hi
     </div>
   );
 }
