12d8012cb990752e2d71e20d1d98c5e431ac8c2b
DuaaH <dodohalaiqa.1@gmail.com>
2020-05-10 17:24:23 +0300

    create react app

---
+++ material_ui_react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import NavBar from '../src/components/NavBar'
+import CourseList from '../src/components/CourseList'
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <CourseList />
+    </div>
+  );
+}
+
+export default App;
---
+++ material_ui_react/src/courses.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    title: "React",
+    description:
+      "React is a JavaScript library for building user interfaces.  ",
+    imageUrl:
+      "https://hackernoon.com/hn-images/1*EntHChgUyirgbZ9A3zTxkA.png"
+  },
+  {
+    title: "Redux",
+    description:
+      "Redux is a predictable state container for JavaScript apps.",
+    imageUrl:
+      "https://booklovinmamas.com/wp-content/uploads/2018/06/redux.jpg"
+  },
+  {
+    title: "Materil UI",
+
+    description:
+      "React components for faster and easier web development. Build your own design system.",
+    imageUrl:
+      "https://i.ytimg.com/vi/_2OKepOJrJs/hqdefault.jpg"
+  }
+];
