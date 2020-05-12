951587ddbbb4359d1c5c6c0420a216f5059eb259
DuaaH <dodohalaiqa.1@gmail.com>
2020-05-12 18:08:49 +0300

    fixed bug

--- server/app.js
+++ server/app.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const path = require("path");
+console.log("hi");
 
 const app = express();
-app.use(express.static(path.join(__dirname, "..", "darbapp", "build")));
+app.use(express.static(path.join(__dirname, "..", "material_ui_react", "build")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use("/api", router);
-
-// if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "material_ui_react", "build", "index.html"));
 });
